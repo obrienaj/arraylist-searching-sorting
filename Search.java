a92360639b6eaa7879d4db560a0f54d07367a27b
Allie O'Brien <obrienaj@s.dcsdk12.org>
2020-04-14 22:08:46 -0600

    Finish binarySearch algorithm

--- Search.java
+++ Search.java
@@ -6,7 +6,7 @@ public class Search{
         "banana", "banana", "cherry", "lemon", "lime", "mango", "orange", 
         "pear", "strawberry", "watermelon"));
 
-        System.out.println(search(fruits, "blueberry"));
+        System.out.println(binarySearch(fruits, "watermelon"));
     }
 
     public static int search(ArrayList<String> haystack, String needle){
@@ -19,6 +19,38 @@ public class Search{
     }
 
     public static int binarySearch(ArrayList<String> haystack, String needle){
+        int midIndex, i;
+        int from = 0;
+        int to = haystack.size();
+
+        while ((to - from) > 1){
+            
+            midIndex = from + ((to - from) / 2);
+            
+            i = needle.compareTo(haystack.get(midIndex));
+            
+            System.out.println("to = " + to);
+            System.out.println("from = " + from);
+            System.out.println("i = " + i);
+
+            if (i == 0){
+                return midIndex;
+            }
+
+            if (i > 0){
+                from = midIndex + 1;
+            }
+
+            if (i < 0){
+                to = midIndex - 1;
+            }
+        }
+        if (needle.compareTo(haystack.get(to)) == 0){
+            return to;
+        }
+        if (needle.compareTo(haystack.get(from)) == 0){
+            return from;
+        }
         return -1;
     }
 }
